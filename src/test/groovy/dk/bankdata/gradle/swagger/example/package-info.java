5572369dd7ac466c580b32f46f6bbd9b6a13360d
Thor Anker Kvisgård Lange <tel@bankdata.dk>
2017-06-22 09:33:18 +0200

    Just a small description of the example model.

---
+++ src/test/groovy/dk/bankdata/gradle/swagger/example/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains some simple JAX-RS endpoints and a corresponding POJO model
+ * to be used for component/integration test of the Swagger plugin. The model is simulating
+ * a fictional account and transaction model.
+ */
+package dk.bankdata.gradle.swagger.example;
